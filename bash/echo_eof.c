e4560e9b83509e8e1ef1bec00407add48e0d4e9c
Dmitriy Fedorov <dmitriy.fedoroff@gmail.com>
2020-08-15 15:51:34 +0300

    запилил первый блокнот в bash

---
+++ bash/echo_eof.c
@@ -0,0 +1,11 @@
+/* echo_eof.c -- repeats input to end of file */
+#include <stdio.h>
+int main(void)
+{
+    int ch;
+
+    while ((ch = getchar()) != EOF) // -1
+      putchar(ch);
+
+    return 0;
+}
